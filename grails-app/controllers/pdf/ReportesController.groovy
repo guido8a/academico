651f6f3e85436747051b7c1f4497417332c7043e
Fabricio <fegrijalva2501@hotmail.com>
2024-02-22 15:20:55 -0500

    reporte cabecera

--- grails-app/controllers/pdf/ReportesController.groovy
+++ grails-app/controllers/pdf/ReportesController.groovy
@@ -2828,18 +2828,19 @@ class ReportesController {
                 suma += r?.hora?.toDouble() * r?.asignatura?.factorPreparacion?.toDouble() ?: r?.hora?.toDouble()
             }
 
-
+            if(suma > 0){
             Row rowF1t = sheet.createRow(fila)
             rowF1t.createCell(18).setCellValue("Total")
             rowF1t.createCell(19).setCellValue(suma)
             rowF1t.setRowStyle(style)
-
             fila++
+            }
 
-            Row rowFilaVacia = sheet.createRow(fila)
-            rowFilaVacia.createCell(0).setCellValue("")
-            rowFilaVacia.setRowStyle(style)
-
+            if(cabecera == 'true') {
+                Row rowFilaVacia = sheet.createRow(fila)
+                rowFilaVacia.createCell(0).setCellValue("")
+                rowFilaVacia.setRowStyle(style)
+            }
 
             if ((suma > 40) && (periodo.tipo == 'N')) {
                 if(cabecera == 'true'){
